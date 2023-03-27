0f06c8688fc75fb0b22310442d2a7020f53d71a4
colinbondi <bodhidude@icloud.com>
2023-03-27 13:56:48 -0700

    Implement the edit and exit buttons

--- gui.py
+++ gui.py
@@ -9,10 +9,14 @@ list_box = sg.Listbox(values=functions.get_todos(),
                       enable_events=True,
                       size=(45, 10))
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 
 window = sg.Window("Todo App",
-                   layout=[[label, input_box, add_button],
-                   [list_box, edit_button]],
+                   layout=[[label],
+                   [input_box, add_button],
+                   [list_box, edit_button, complete_button],
+                   [exit_button]],
                    font=('Helvetica', 16))
 
 while True:
@@ -29,15 +33,26 @@ while True:
 
         case "Edit":
             todo_to_edit = values["todos"][0]
-            new_todo = values['todo']
+            new_todo = values['todo'] + '\n'
             todos = functions.get_todos()
             index = todos.index(todo_to_edit)
             todos[index] = new_todo
             functions.write_todos(todos)
             window['todos'].update(values=todos)
 
+        case "Complete":
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].Update(value="")
+
         case "todos":
             window['todo'].update(value=values['todos'][0])
+
+        case "Exit":
+            break
 
         case sg.WIN_CLOSED:
             break
